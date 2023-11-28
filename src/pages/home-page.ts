511cd9f42c0528d51c7bae668ab155f6171e220f
Yvonne Kwarteng <yvonne.kwarteng@centre4.local>
2023-11-28 16:41:08 +0000

    lines added between methods.

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -11,14 +11,14 @@ export class HomePage extends BasePage {
       techAcademyLink: this.page.getByRole('link', { name: 'Tech Academy' }).nth(0),
       contactLink: this.page.getByRole('link', { name: 'Contact' }).nth(0),
       consultantServicesLink: this.page.getByText('Consultancy Services').nth(0),
-      qualityEngineeringLink: this.page.getByText('Quality Engineering').nth(0),
+      qualityEngineeringLink: this.page.getByText('Quality Engineering').nth(0)
     };
   }
 
   async clickTechAcademyLink() {
     await this.elements.techAcademyLink.click();
   }
-  
+
   async checkTechAcademyPageTitle() {
     await this.clickTechAcademyLink();
     await expect(this.page).toHaveTitle('Tech Academy - Ten10');
@@ -36,14 +36,16 @@ export class HomePage extends BasePage {
   async clickConsultantServicesLink() {
     await this.elements.consultantServicesLink.click();
   }
+  
   async clickQualityEngineeringLink() {
     await this.elements.qualityEngineeringLink.click();
   }
 
   async getGualityEngineeringLink() {
-  this.clickConsultantServicesLink();
+    await this.clickConsultantServicesLink();
     await this.elements.qualityEngineeringLink.click();
   }
+
   async checkQualityEngineeringPageTitle() {
     await this.clickQualityEngineeringLink();
     await expect(this.page).toHaveTitle('Contact-Ten10');
