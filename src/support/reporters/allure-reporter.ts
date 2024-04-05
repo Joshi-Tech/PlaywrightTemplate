72b9272aa98ba80954430d41359a0ece26335ed7
Yvonne Kwarteng <yvonne.kwarteng@centre4.local>
2024-04-05 10:04:41 +0100

    allure update

--- src/support/reporters/allure-reporter.ts
+++ src/support/reporters/allure-reporter.ts
@@ -1,29 +1,60 @@
-import { AllureRuntime, CucumberJSAllureFormatter } from 'allure-cucumberjs';
+import { AllureRuntime } from "allure-js-commons";
+import { CucumberJSAllureFormatter, CucumberJSAllureFormatterConfig } from "allure-cucumberjs";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function Reporter(options: any) {
-  return new CucumberJSAllureFormatter(
-    options,
-    new AllureRuntime({ resultsDir: './reports/allure-results' }),
-    {
-      labels: {
-        epic: [/@feature:(.*)/],
-        severity: [/@severity:(.*)/],
-      },
-      links: {
-        issue: {
-          pattern: [/@issue=(.*)/],
-          urlTemplate: 'http://localhost:8080/issue/%s',
-        },
-        tms: {
-          pattern: [/@tms=(.*)/],
-          urlTemplate: 'http://localhost:8080/tms/%s',
-        },
-      },
-    },
-  );
+interface CustomLabelMatcher {
+  pattern: RegExp[];
+  name: string;
 }
-Reporter.prototype = Object.create(CucumberJSAllureFormatter.prototype);
-Reporter.prototype.constructor = Reporter;
 
-exports.default = Reporter;
+interface CustomLinkMatcher {
+  pattern: RegExp[];
+  type: string;
+  urlTemplate: string;
+}
+
+interface CustomCucumberJSAllureFormatterConfig extends CucumberJSAllureFormatterConfig {
+  labels: CustomLabelMatcher[];
+  links: CustomLinkMatcher[];
+}
+
+export default class CustomAllureReporter extends CucumberJSAllureFormatter {
+  constructor(options: any) {
+    super(
+      options,
+      new AllureRuntime({ resultsDir: "./allure-results" }),
+      {
+        labels: [
+          {
+            pattern: [/@feature:(.*)/],
+            name: "epic",
+          },
+          {
+            pattern: [/@severity:(.*)/],
+            name: "severity",
+          },
+        ],
+        links: [
+          {
+            pattern: [/@issue=(.*)/],
+            type: "issue",
+            urlTemplate: "http://localhost:8080/issue/%s",
+          },
+          {
+            pattern: [/@tms=(.*)/],
+            type: "tms",
+            urlTemplate: "http://localhost:8080/tms/%s",
+          },
+        ],
+      } as CustomCucumberJSAllureFormatterConfig
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
